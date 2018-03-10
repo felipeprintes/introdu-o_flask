5df2dc57504b1370470180505053eae8f292c930
felipe <felipe.unilasalle@gmail.com>
2018-03-09 23:15:16 -0300

    introdução ao flask

--- jogoteca.py
+++ jogoteca.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')
+    lista =["jogo1", "jogo2", "jogo3"]
+    return render_template('lista.html', titulo="Meus Jogos", meusJogos=lista)
+
 
 app.run()
