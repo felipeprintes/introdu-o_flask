99515835ac0cbcad9ca53311b02eb27cb7008416
felipe <felipe.unilasalle@gmail.com>
2018-03-10 12:48:45 -0300

    intro flask

--- jogoteca.py
+++ jogoteca.py
@@ -1,10 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+class Jogo:
+    def __init__(self,nome,categoria, console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
+
+jogo1=Jogo("mario", "aventura", "nintendo")
+jogo2=Jogo("fifa", "esporte", "play4")
+lista =[jogo1,jogo2]
+
 @app.route('/inicio')
 def ola():
-    lista =["jogo1", "jogo2", "jogo3"]
+
     return render_template('lista.html', titulo="Meus Jogos", meusJogos=lista)
 
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar')
+def criar():                    #pegar dados vindo do servidor
+    nome=request.fomr['nome']
+    categoria=request.fomr['categoria']
+    console=request.fomr['console']
+    jogo = Jogo(nome, categoria,console)
+    lista.append(jogo)
 
 app.run()
